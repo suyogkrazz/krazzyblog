6b9d41dc498e021d862f60379e93ec65ddac7611
suyog <suyog@vayu.com.au>
2018-04-16 15:28:07 +0545

    commit init

---
+++ content/meta/config.js
@@ -0,0 +1,30 @@
+const colors = require("../../src/styles/colors");
+
+module.exports = {
+  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
+  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
+  siteDescription: "PersonalBlog is a GatsbyJS starter.",
+  siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
+  pathPrefix: "",
+  siteImage: "preview.jpg",
+  siteLanguage: "en",
+  // author
+  authorName: "Suyog K.C",
+  authorTwitterAccount: "suyogkrazz",
+  // info
+  infoTitle: "Suyog K.C",
+  infoTitleNote: "personal blog",
+  // manifest.json
+  manifestName: "PersonalBlog - Suyog K.C",
+  manifestShortName: "PersonalBlog", // max 12 characters
+  manifestStartUrl: "/",
+  manifestBackgroundColor: colors.bg,
+  manifestThemeColor: colors.bg,
+  manifestDisplay: "standalone",
+  // social
+  authorSocialLinks: [
+    { name: "github", url: "https://github.com/greglobinski" },
+    { name: "twitter", url: "https://twitter.com/greglobinski" },
+    { name: "facebook", url: "http://facebook.com/greglobinski" }
+  ]
+};
