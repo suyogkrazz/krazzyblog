a6c66f7cc73d416ca7ad2a25e32c815a2b18b92f
suyog <suyog@vayu.com.au>
2018-04-27 01:32:42 +0545

    about me update

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,15 +4,41 @@ import PropTypes from "prop-types";
 import Article from "../Main/Article";
 import PageHeader from "./PageHeader";
 import Content from "../Main/Content";
+import { BarChart, XAxis, Tooltip, Legend, YAxis, CartesianGrid, Bar } from "recharts";
 
 const Page = props => {
   const { page } = props;
   const html = (page || {}).html;
-
+  const data = [
+    { name: "Javascript", percent: 80 },
+    { name: "Laravel", percent: 70 },
+    { name: "ROR", percent: 50 },
+    { name: "Angular", percent: 70 },
+    { name: "ReactJS", percent: 70 },
+    { name: "VueJS", percent: 60 },
+    { name: "React Native", percent: 70 }
+  ];
   return (
     <Article>
       <PageHeader {...page.frontmatter} />
       <Content html={html} />
+      {page.frontmatter.title == "About" && (
+        <div>
+          <h3>My Skills</h3>
+          <BarChart
+            width={600}
+            height={300}
+            data={data}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <Legend />
+            <Tooltip />
+            <Bar dataKey="percent" fill="#8884d8" />
+          </BarChart>
+        </div>
+      )}
     </Article>
   );
 };
